ad267c8119d9fbcee426aa1204a0fff418631f94
Tanweer Anwar <48026791+tanweeranwar@users.noreply.github.com>
2026-08-30 02:26:16 -0400

    fix: import branch authorization service

--- Laundry.API/Controllers/BranchPricingController.cs
+++ Laundry.API/Controllers/BranchPricingController.cs
@@ -1,5 +1,6 @@
 using Laundry.API.DTOs.BranchPricing;
 using Laundry.API.Interfaces;
+using Laundry.API.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
